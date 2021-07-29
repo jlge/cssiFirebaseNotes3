deac91f1529dff544edaa727e476284fd69cf23c
Joanna Ge <joannage87@gmail.co>
2021-07-29 18:09:54 +0000

    Add edit and delete functionality

--- public/js/viewNotes.js
+++ public/js/viewNotes.js
@@ -1,9 +1,11 @@
+let googleUserId;
+
 window.onload = (event) => {
   // Use this to retain user state between html pages.
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       console.log('Logged in as: ' + user.displayName);
-      const googleUserId = user.uid;
+      googleUserId = user.uid;
       getNotes(googleUserId);
     } else {
       // If not logged in, navigate back to login page.
@@ -22,16 +24,16 @@ const getNotes = (userId) => {
 
 const renderDataAsHtml = (data) => {
   let cards = ``;
-  for(const noteItem in data) {
-    const note = data[noteItem];
+  for(const noteId in data) {
+    const note = data[noteId];
     // For each note create an HTML card
-    cards += createCard(note)
+    cards += createCard(note, noteId)
   };
   // Inject our string of HTML into our viewNotes.html page
   document.querySelector('#app').innerHTML = cards;
 };
 
-const createCard = (note) => {
+const createCard = (note, noteId) => {
    return `
      <div class="column is-one-quarter">
        <div class="card">
@@ -41,7 +43,45 @@ const createCard = (note) => {
          <div class="card-content">
            <div class="content">${note.text}</div>
          </div>
+         <footer class="card-footer">
+            <a id="${noteId}" href="#" class="card-footer-item" onclick="deleteNote('${noteId}')">Delete</a>
+            <a href="#" class="card-footer-item" onclick="editNote('${noteId}')">Edit</a>
+         </footer>
        </div>
      </div>
    `;
 };
+
+function deleteNote(noteId) {
+    console.log("deleteNote() ", noteId);
+    firebase.database().ref(`users/${googleUserId}/${noteId}`).remove()
+}
+
+function editNote(noteId) {
+    const editNoteModal = document.querySelector('#editNoteModal');
+
+    const notesRef = firebase.database().ref(`users/${googleUserId}/${noteId}`);
+    notesRef.on('value', (snapshot) => {
+        const note = snapshot.val();
+        document.querySelector('#editTitleInput').value = note.title;
+        document.querySelector('#editTextInput').value = note.text;
+        document.querySelector("#noteId").value = noteId;
+    });
+    editNoteModal.classList.toggle('is-active');
+}
+
+function saveEditedNote() {
+    console.log("hi");
+    const title = document.querySelector('#editTitleInput').value;
+    const text = document.querySelector("#editTextInput").value;
+    const noteId = document.querySelector("#noteId").value;
+    const editedNote = {title, text};
+    firebase.database().ref(`users/${googleUserId}/${noteId}`).update(editedNote);
+    closeEditModal();
+}
+
+function closeEditModal() {
+    console.log("close ");
+    const editNoteModal = document.querySelector('#editNoteModal');
+    editNoteModal.classList.toggle("is-active");
+}
